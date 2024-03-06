1c29dbef2fd1b2770ec13502aabfe2d2832e92f2
allengabriel134 <allendave.gabriel@xdnainteractive.com>
2024-03-06 13:13:33 +0800

    Fixed register page

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -26,48 +26,51 @@ const Register = () => {
         }
     }
     return (
-        <div>
-            {userLoggedIn && <Navigate to={'/dashboard'} replace={true} />}
-
-            <main classname="register">
-    <div className="container">
-        <div className="register_left">
-            <img src={rvmpic1} alt="reg" className="reg_icon" />
-            <hr className="separator"/>
-        </div>
-        <div className="right-content">
-                        <h3 className="header_register">Create a New Account</h3>
-                        <form onSubmit={onSubmit}>
-                            <div className="reg_inputs">
-                                <div className="reg_input">
-                                <img src={email_icon} alt=""/>
-                                    <input type="email" placeholder="Institutional Email" autoComplete="email"  required value={email} onChange={(e) => setEmail(e.target.value)} />
-                                </div>
-                                <div className="reg_input">
-                                <img src={student_icon} alt=""/>
-                                    <input type="text" autoComplete='off' placeholder="Student / Employee Number" required value={studentNumber} onChange={(e) => setStudentNumber(e.target.value)} />
-                                </div>
-                                <div className="reg_input">
-                                <img src={password_icon} alt=""/>
-                                    <input type="password" autoComplete="new-password" placeholder="Password" disabled={isRegistering} required value={password} onChange={(e) => setPassword(e.target.value)} />
-                                </div>
-                                <div className="reg_input">
-                                <img src={cpass_icon} alt=""/>
-                                    <input type="password" autoComplete="off" placeholder="Confirm Password" disabled={isRegistering} required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                                </div>
+        // <div>
+          
+            <section className="register">
+                {userLoggedIn && <Navigate to={'/dashboard'} replace={true} />}
+                <div className='outer-wrapper'>
+                    <div className="inner-wrapper">
+                            <div className="register_left">
+                                <img src={rvmpic1} alt="reg" className="reg_icon" />
+                                <hr className="reg-separator"/>
                             </div>
-                            {errorMessage && <span className="forgot-password">{errorMessage}</span>}
-                            <div className="submit-container">
-                                <button type="submit" className="submit" disabled={isRegistering}>
-                                    {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                                </button>
+                            <div className="right-content">
+                                <h3 className="header_register">Create a New Account</h3>
+                                <form onSubmit={onSubmit}>
+                                    <div className="reg_inputs">
+                                        <div className="reg_input">
+                                        <img src={email_icon} alt=""/>
+                                            <input type="email" placeholder="Institutional Email" autoComplete="email"  required value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        </div>
+                                        <div className="reg_input">
+                                        <img src={student_icon} alt=""/>
+                                            <input type="text" autoComplete='off' placeholder="Student / Employee Number" required value={studentNumber} onChange={(e) => setStudentNumber(e.target.value)} />
+                                        </div>
+                                        <div className="reg_input">
+                                        <img src={password_icon} alt=""/>
+                                            <input type="password" autoComplete="new-password" placeholder="Password" disabled={isRegistering} required value={password} onChange={(e) => setPassword(e.target.value)} />
+                                        </div>
+                                        <div className="reg_input">
+                                        <img src={cpass_icon} alt=""/>
+                                            <input type="password" autoComplete="off" placeholder="Confirm Password" disabled={isRegistering} required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                                        </div>
+                                    </div>
+                                    {errorMessage && <span className="forgot-password">{errorMessage}</span>}
+                                    <div className="submit-container">
+                                        <button type="submit" className="submit" disabled={isRegistering}>
+                                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
+                                        </button>
+                                    </div>
+                                </form>
+                                <p>Already have an account? <Link to="/login">Sign in</Link></p>
+                        
                             </div>
-                        </form>
-                        <p>Already have an account? <Link to="/login">Sign in</Link></p>
                     </div>
                 </div>
-            </main>
-        </div>
+            </section>
+        // </div>
     );
 };
 
