dc9417729f9e095ae5703eb8b2c582e9f220a64d
Danica Batalla <danicabatalla0323@gmail.com>
2024-03-06 14:51:03 +0800

    fixed routing

--- src/contexts/authContext/index.jsx
+++ src/contexts/authContext/index.jsx
@@ -57,15 +57,9 @@ export function AuthProvider({ children }) {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && userLoggedIn ? (
+      {userLoggedIn ? (
         <Sidebar>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/rewards" element={<Rewards />} />
-            <Route path="/TransactionHistory" element={<TransactionHistory />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
+          {children}
         </Sidebar>
       ) : children
       }
