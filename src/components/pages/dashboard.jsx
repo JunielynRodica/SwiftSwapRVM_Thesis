c2bd8da86f6096ea26a4fa2728be339e69fb9598
Danica Batalla <danicabatalla0323@gmail.com>
2024-03-11 17:09:25 +0800

    modified app.css

--- src/components/pages/dashboard.jsx
+++ src/components/pages/dashboard.jsx
@@ -26,22 +26,14 @@ const Dashboard = () => {
       <body>
         <div className='user'>
           <div>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, it's nice to see you again!</div>
+          <div>SwiftSwap Points: 50</div>
           {/* <p className='user-welcome'>WELCOME BACK: USER 384782972</p>
             <p className='user-point'>SwiftSwap Points: 50</p> */}
         </div>
-        <section className="intro">
-          <h1 className="welcome">HI, WHAT ARE YOU RECYCLING TODAY?</h1>
-          <p className="sentence1">We're excited for you to experience our project! Simply follow the instructions below and start swapping now!</p>
-        </section>
-        <div className="how-to">
-          <img src={step1} alt="step1"></img>
-          <img src={step2} alt="step2"></img>
-          <img src={step3} alt="step3"></img>
-          <img src={step4} alt="step4"></img>
-          <img src={step5} alt="step5"></img>
-          <img src={step6} alt="step6"></img>
-        </div>
-
+        <br></br>
+        
+        <p className="qr-intro">Use this QR Code to Log in on the SwiftSwap Machine.</p>
+        <br></br>
         <div style={{ height: "auto", margin: "0 auto", maxWidth: 300, width: "100%" }}>
           {
             QRCreds ? <QRCode
@@ -52,7 +44,21 @@ const Dashboard = () => {
             /> : <p className="sentence1">Failed to generate QR, please re-login.</p>
           }
         </div>
+        <br></br>
 
+        <section className="intro">
+          <h1 className="welcome">HI, WHAT ARE YOU RECYCLING TODAY?</h1>
+          <p className="sentence1">We're excited for you to experience our project! Simply follow the instructions below and start swapping now!</p>
+        </section>
+
+        <div className="how-to">
+          <img src={step1} alt="step1"></img>
+          <img src={step2} alt="step2"></img>
+          <img src={step3} alt="step3"></img>
+          <img src={step4} alt="step4"></img>
+          <img src={step5} alt="step5"></img>
+          <img src={step6} alt="step6"></img>
+        </div>
       </body>
     </div>
   );
