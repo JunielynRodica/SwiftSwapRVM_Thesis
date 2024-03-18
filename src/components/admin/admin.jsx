d8fda9621349e307333502b2f32d5d53767c74bf
JunielynRodica <159602904+JunielynRodica@users.noreply.github.com>
2024-03-18 18:25:24 +0800

    added admin page

---
+++ src/components/admin/admin.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import registered_users from '../../../assets/registered_user.png';
+
+const Admin = () => {
+    return (
+        <div className="admin_body">
+        <div className="admin_rewards">
+          <img src={rvmpic} alt="logo" />
+          <h1>S W I F T S W A P | A D M I N </h1>
+        </div>
+ 
+    <section id="admin_feature">
+            <div className="access">
+            <img src={registered_users} alt=""/>
+            <button className="user-access-btn" onClick={handleAdminAccess}>Access</button>
+              </div>
+    
+            <div className="access">
+            <img src={user_transaction} alt=""/>
+            <button className="user-transac-btn" onClick={handleAdminAccess}>Access</button>
+              </div>
+          </section>
+        </div>
+       
+     // <div className="user-table-container">
+        //   <h2>User Logs</h2>
+        //   <div className="user-table-wrapper">
+        //     <table className="user-table">
+        //       <thead>
+        //         <tr>
+        //           <th>Employee/ Student Number</th>
+        //           <th>Email</th>
+        //           <th>User ID</th>        
+        //         </tr>
+        //       </thead>
+        //     </table>
+        //   </div>
+        // </div>
+
+        // <div className="transaction-table-container">
+        //   <h2>Transaction Histories</h2>
+        //   <div className="transaction-table-wrapper">
+        //     <table className="transaction-table">
+        //       <thead>
+        //         <tr>
+        //           <th>User</th>
+        //           <th>Date</th>
+        //           <th>Time</th>
+        //           <th>Action</th>
+        //           <th>Points</th>
+        //           <th>Item Redeemed</th>
+        //         </tr>
+        //       </thead>
+        //       <tbody>
+        //           { hasData ? output : <tr><td colSpan="5">No transaction history available</td></tr> }
+        //       </tbody>
+        //     </table>
+        //   </div>
+        // </div>
+      );
+    };
+
+    export default Admin;
