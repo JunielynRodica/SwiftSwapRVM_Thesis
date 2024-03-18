12373b5ce39b48a1ca51fb5c2aeb3c4c50f75914
Danica Batalla <danicabatalla0323@gmail.com>
2024-03-18 16:31:41 +0800

    forgot password page content and design added

--- src/components/auth/forgotpassword/index.jsx
+++ src/components/auth/forgotpassword/index.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate, Link, useSearchParams } from 'react-router-dom';
+import rvmpic from '../../../assets/rvmpic.png';
+import emailIcon from '../../../assets/Email.png';
+import '../../../style/forgotpw.css'
 
 import { useAuth } from '../../../contexts/authContext';
 
@@ -7,12 +10,36 @@ const ForgotPassword = () => {
     const { userLoggedIn } = useAuth();
 
     return (
-        <div>
+        <main>
             {userLoggedIn && <Navigate to={'/dashboard'} replace={true} />}
-            <main>
+            <div className='forgotContainer'>
+                <div className='forgotHeader'>
+                    <img src={rvmpic} className='forgotLogo'></img>
+                    <h1 className='forgotTitle'>S w i f t S w a p</h1>
+                    <h2>Please provide your email address. We'll send you a link to your email to reset your password in just a few moments.</h2>
+                    <br></br>
+                    <div className='forgetInput'>
+                    <img src={emailIcon}></img>
+                    <input className='forgetEmail' type='email' required placeholder='Enter Email here.' ></input>
+                    <input className='forgotButton' type='submit' value='Reset Password'></input>
+                    </div>
+                </div>
+            </div>
 
-            </main>
-        </div>
+            {/* //for next page// 
+            <div className='resetContainer'>
+            <h2>Reset your Password</h2>
+                <div className="resetInput">
+                    <img src={passwordIcon}></img>
+                    <input className="resetNew" type='password' required placeholder='Enter New Password'></input>
+                    <p className="resetText">Password must be atleast 6 characters.</p>
+                    <img src={confirmIcon}></img>
+                    <input className="confirmNew" type='password' required placeholder='Confirm New Passowrd'></input>
+                    <input classname='resetButton' type='submit' value='Reset Password'></input>
+                </div>
+            </div> */}
+            
+        </main>
     );
 };
 
